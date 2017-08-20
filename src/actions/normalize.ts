aefa13f145e3d7b9fa39a084664f0d869b7a976e
Michael Krone <michael.krone@outlook.com>
2017-08-20 23:48:47 +0200

    feat: rename reducer function, add documentation

--- src/actions/normalize.ts
+++ src/actions/normalize.ts
@@ -1,36 +1,131 @@
+/**
+ * Exports actions and an actionCreators creator of the ngrx-normalizr package.
+ */
+
 import { Action } from '@ngrx/store';
 import { schema } from 'normalizr';
 
+/**
+ * Internal action namespace
+ */
 const ACION_NAMESPACE = '[@@Normalize]';
 
+/**
+ * A map of schema names to object property names.
+ * Used for removing child properties of an entity.
+ */
 export interface SchemaMap {
 	[schemaKey: string]: string;
 }
 
+/**
+ * Base interface for `SetData` and `RemoveData` action payload.
+ */
 export interface SchemaPayload {
+	/**
+   * Schema definition of the entity. Used for de-/ and normalizing given entities.
+   */
 	schema: schema.Entity;
 }
 
+/**
+ * Typed Interface for the payload of the `SetData` action.
+ * Accepts an typed array of entities to be added to the store.
+ */
 export interface NormalizeSetDataPayload<T> extends SchemaPayload {
+	/**
+   * The array of entities which should be normalized and added to the store.
+   */
 	data: T[];
 }
 
+/**
+ * Interface for the payload of the `RemoveData` action.
+ * Accepts an `id` and an optional `removeChildren` property.
+ */
 export interface NormalizeRemovePayload extends SchemaPayload {
+	/**
+   * The id of the entity that should be removed
+   */
 	id: string;
+
+	/**
+   * If set and maps to valid schema keys and propety names,
+   * children referenced by the entity will be removed
+   */
 	removeChildren?: SchemaMap;
 }
 
+/**
+ * All types of the provided actions.
+ */
 export class NormalizeActionTypes {
+	/**
+   * Action type of the `SetData` action.
+   */
 	static readonly SET_DATA = `${ACION_NAMESPACE} Set Data`;
+
+	/**
+   * Action type of the `RemoveData` action.
+   */
 	static readonly REMOVE_DATA = `${ACION_NAMESPACE} Remove Data`;
 }
 
+/**
+ * Action for setting/updating data to the store.
+ * Also see `NormalizeSetDataPayload`.
+ */
 export class SetData<T> implements Action {
+	/**
+   * The action type: `NormalizeActionTypes.SET_DATA`
+   */
 	readonly type = NormalizeActionTypes.SET_DATA;
+
+	/**
+   * SetData Constructor
+   * @param payload The action payload used in the reducer
+   */
 	constructor(public payload: NormalizeSetDataPayload<T>) {}
 }
 
+/**
+ * Action for removing data from the store.
+ * Also see `NormalizeRemovePayload`.
+ */
 export class RemoveData implements Action {
+	/**
+   * The action type: `NormalizeActionTypes.REMOVE_DATA`
+   */
 	readonly type = NormalizeActionTypes.REMOVE_DATA;
+
+	/**
+   * RemoveData Constructor
+   * @param payload The action payload used in the reducer
+   */
 	constructor(public payload: NormalizeRemovePayload) {}
+}
+
+/**
+ * Create a set of action creators for the `SetData` and `RemoveData` actions.
+ * This is provided for convenience.
+ * @param schema The schema the action creators should be bound to
+ */
+export function actionCreators<T>(schema: schema.Entity) {
+	return {
+		/**
+     * Action creator for the `SetData` action.
+     * @returns A new instance of the `SetData` action with the given schema set.
+     */
+		setData: (data: NormalizeSetDataPayload<T>['data']) =>
+			new SetData<T>({ data, schema }),
+
+		/**
+     * Action creator for the `RemoveData` action.
+     * @returns A new instance of the `RemoveData` action with the given schema set.
+     */
+		removeData: (
+			id: NormalizeRemovePayload['id'],
+			removeChildren?: NormalizeRemovePayload['removeChildren']
+		) => new RemoveData({ id, schema, removeChildren })
+	};
 }
--- src/reducers/normalize.ts
+++ src/reducers/normalize.ts
@@ -1,3 +1,7 @@
+/**
+ * Exports reducers and selectors of the ngrx-normalizr package.
+ */
+
 import { createSelector, MemoizedSelector } from '@ngrx/store';
 import { normalize, denormalize, schema } from 'normalizr';
 
@@ -6,35 +10,48 @@ import { NormalizeActionTypes } from '../actions/normalize';
 /**
  * The state key under which the normalized state will be stored
  */
-export const STATE_KEY = '@@normalized';
+const STATE_KEY = 'normalized';
 
 /**
- * A map of scheka keys to a map of entity id's to entity data
+ * Interface describing the entities propery of a normalized state.
+ * A map of schema keys wich map to a map of entity id's to entity data.
+ * This corresponds to the `entities` property of a `normalizr.normalize` result. *
  */
 export interface EntityMap {
 	[key: string]: { [id: string]: any };
 }
 
 /**
- * The state interface from which the app state should extend
+ * The state interface from which the app state should extend.
+ * Holds an instance of `NormalizedEntityState` itself.
  */
 export interface NormalizedState {
 	/** The normalized state property */
-	'@@normalized': NormalizedEntityState;
+	normalized: NormalizedEntityState;
 }
 
 /**
- * The normalized state, representing a normalizr result
+ * The normalized state, representing a `normalizr.normalize` result.
+ * Can be selected by the provided `getNormalizedEntities` and `getResult`
+ * selectors.
  */
 export interface NormalizedEntityState {
-	/** The original sorting of the unnormalized data */
+	/**
+   * The original sorting of the unnormalized data.
+   * Holds all id's of the last set operation in original order.
+   * Can be used to restore the original sorting of entities
+   */
 	result: string[];
-	/** The normalized result */
+
+	/**
+   * The normalized entities. Should be passed to all projector functions
+   * to enable access to all entities needed.
+   */
 	entities: EntityMap;
 }
 
 /**
- * An initial state for the normalized state
+ * The initial state for the normalized entity state.
  */
 const initialState: NormalizedEntityState = {
 	result: [],
@@ -42,15 +59,25 @@ const initialState: NormalizedEntityState = {
 };
 
 /**
- * The normalizing reducer function which will listen to `NormalizeActionTypes.SET_DATA`
- * and `NormalizeActionTypes.REMOVE_DATA`.
+ * The normalizing reducer function which will handle actions with the types
+ * `NormalizeActionTypes.SET_DATA` and `NormalizeActionTypes.REMOVE_DATA`.
  *
- * `action.payload` can hold different schemed entities which could have been updated/added
+ * On an `NormalizeActionTypes.SET_DATA` action:
+ *
+ * Entities are identified by their id attribute set in the schema passed by the payload.
+ * Existing entities will be overwritten by updated data, new entities will be added to the store.
+ *
+ * On an `NormalizeActionTypes.REMOVE_DATA` action:
+ *
+ * Entities are identified by their id attribute set in the schema passed by the payload.
+ * The entity with the passed id will be removed. If a `removeChildren` option is set in the action
+ * payload, it is assumed as a map of schema keys to object property names. All referenced children
+ * of the entity will be read by the object propety name and removed by the schema key.
  *
  * @param state The current state
- * @param action The dispatched action
+ * @param action The dispatched action, one of `NormalizeActionTypes.SET_DATA` or `NormalizeActionTypes.REMOVE_DATA`.
  */
-export function reducer(
+export function normalized(
 	state: NormalizedEntityState = initialState,
 	action: any
 ) {
@@ -105,9 +132,18 @@ export function reducer(
 	}
 }
 
+/**
+ * Default getter for the normalized state
+ * @param state any state
+ */
 const getNormalizedState = (state: any): NormalizedEntityState =>
 	state[STATE_KEY];
 
+/**
+ * Selects all normalized entities of the state, regardless of their schema.
+ * This selector should be used to enable denormalizing projector functions access
+ * to all needed schema entities.
+ */
 export const getNormalizedEntities: MemoizedSelector<
 	any,
 	EntityMap
@@ -116,16 +152,38 @@ export const getNormalizedEntities: MemoizedSelector<
 	(state: NormalizedEntityState) => state.entities
 );
 
+/**
+ * Select the result order of the last set operation.
+ */
 export const getResult: MemoizedSelector<any, string[]> = createSelector(
 	getNormalizedState,
 	(state: NormalizedEntityState) => state.result
 );
 
+/**
+ * Creates an object of selectors and projector functions bound to the given schema.
+ * @param schema The schema to bind the selectors and projectors to
+ */
 export function createSchemaSelectors<T>(schema: schema.Entity) {
 	return {
+		/**
+     * Select all entities, regardless of their schema, exported for convenience.
+     */
 		getNormalizedEntities,
+
+		/**
+     * Select all entities and perform a denormalization based on the given schema.
+     */
 		getEntities: createEntitiesSelector<T>(schema),
+
+		/**
+     * Uses the given schema to denormalize an entity by the given id
+     */
 		entityProjector: createEntityProjector<T>(schema),
+
+		/**
+     * Uses the given schema to denormalize all given entities
+     */
 		entitiesProjector: createEntitiesProjector<T>(schema)
 	};
 }
